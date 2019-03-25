d71c701d3483539fec2af2bfaff8d8498700ab78
c-imaizumi <c-imaizumi@mediba.jp>
2019-03-25 18:16:46 +0900

    前回の担当者をウィンドウロード時に表示する。参加者リストを作成し、チェックをつけた人の中から抽選する

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div class="seniority"><p>先週の担当者：</p><span>{{ beforePeoplesName }}</span></div>
+    <div class="seniority"><p>先週の担当者：</p><span>{{ beforePeopleNames }}</span></div>
     <div class="dis-f">
       <button @click="localStorageFind()">指名する</button>
       <i class="far fa-hand-point-left click-icon faa-horizontal animated"></i>
@@ -10,85 +10,105 @@
       <p>司会：<span>{{ shikai }}</span></p>
     </div>
     <div class="message">{{ msg }}</div> 
-    
+    <div>
+      <label v-for="(people,index) in initialPeoples" :key="index">
+        <input type="checkbox" v-model="people.checked" @change="peopleFilter" /> {{ people.name }}
+      </label> 
+    </div>
   </div>
 </template>
 
 <script>
+
 const people = [
-  {name: '一朗'},
-  {name: '二郎'},
-  {name: '三郎'},
-  {name: '四郎'},
-  {name: '五郎'},
-  {name: '六郎'},
-  {name: '七郎'},
-  {name: '八郎'},
-  {name: '九郎'},
-  {name: '十郎'},
-  {name: '十一郎'},
-  {name: '十二郎'},
+  {name: '一朗さん', checked: false},
+  {name: '二郎さん', checked: false},
+  {name: '三郎さん', checked: false},
+  {name: '四郎さん', checked: false},
+  {name: '五郎さん', checked: false},
+  {name: '六郎さん', checked: false},
+  {name: '七郎さん', checked: false},
+  {name: '八郎さん', checked: false},
+  {name: '九郎さん', checked: false},
+  {name: '十郎さん', checked: false},
+  {name: '十一郎さん', checked: false}, 
+  {name: '十二郎さん', checked: false},
 ];
 
 export default {
   name: 'app',
   data() {
     return {
+      initialPeoples: people,
       peoples: people,
       syoki: '',
       shikai: '',
       msg: '',
       beforePeoplesList: [],
+      beforePeopleNames:'',
     }
   },
-
- computed: {
-  beforePeoplesName() { //前回の担当者の文字列に','を挿入
-    return this.beforePeoplesList.join(', ');
+  
+  mounted() { // 前回の担当者をブラウザに表示
+    const getData = localStorage.getItem('name'); 
+    const formationText = getData.split(',', 2);
+    this.beforePeopleNames = formationText.join(', ');
   },
- },
- methods: {
-  returnRandomNum(array) { //randomな整数を返す
-    for(let i = array.length -1; i >= 0; i--) {
-      const randomPeoples = Math.floor(Math.random() * (i + 1));
-      [array[i], array[randomPeoples]] = [array[randomPeoples], array[i]];
-    }
-    this.addPeopleName(array);
+  methods: {
+    peopleFilter() { // チェック状態の配列を生成
+      this.peoples = this.initialPeoples.filter(people => people.checked);
+    },
+
+    returnRandomNum(array) { //randomな整数を返す
+      for(let i = array.length -1; i >= 0; i--) {
+        const randomPeoples = Math.floor(Math.random() * (i + 1));
+        [array[i], array[randomPeoples]] = [array[randomPeoples], array[i]];
+      }
+      this.addPeopleName(array);
+    },
+
+    localStorageFind() { //windowload時にlocalstorageのnameキーを見に行く
+      if(!localStorage.getItem('name')) {
+        this.allLottery();
+      } else { //localStorageの文字列をオブジェクトに形成
+        const getData = localStorage.getItem('name'); 
+        const formationText = getData.split(',', 2);
+        const createObj = [{name:formationText[0]},{name:formationText[1]}];
+        this.sortLottery(createObj); 
+      }
+      return;
+    },
+    
+    allLottery() { //全員のオブジェクトから抽選
+      this.returnRandomNum(this.peoples);
+    },
+
+    sortLottery(sort) { //前回の担当を除いたオブジェクトから抽選
+      const peoples = this.peoples;
+      let copyPeoples = [...peoples];
+      let filteredAry;
+      for(var num in sort) {
+        filteredAry = copyPeoples.filter(people => {
+          return people.name !== sort[num].name;
+        });
+        copyPeoples = filteredAry;
+      };
+      this.returnRandomNum(filteredAry);
+    },
+
+    addPeopleName(targetAry) { //viewに担当者を渡し、localstorageに登録
+      this.syoki = targetAry[0].name;
+      this.shikai = targetAry[1].name;
+      this.msg = 'よろしくおねがいしますm(..)m';
+
+      this.beforePeoplesList = [targetAry[0].name, targetAry[1].name];
+
+      const emptyObj = [];
+      emptyObj.push(this.beforePeoplesList);
+      const setItem = JSON.stringify(this.beforePeoplesList);
+      localStorage.setItem('name', this.beforePeoplesList);   
+    },
   },
-
-  localStorageFind() { //windowload時にlocalstorageのnameキーを見に行く
-    if(!localStorage.getItem('name')) {
-      this.allLottery();
-    }else { //localStorageの文字列をオブジェクトに形成
-      const getData = localStorage.getItem('name');
-      const formationText = getData.split(',', 2);
-      const createObj = [{name:formationText[0]},{name:formationText[1]}];
-      this.sortLottery(createObj); 
-    }
-    return;
-  },
-  allLottery() { //全員のオブジェクトから抽選
-    this.returnRandomNum(this.peoples);
-  },
-
-  sortLottery(sort) { //前回の担当を除いたオブジェクトから抽選
-    const sortAry = this.peoples.filter(i => i.name !== sort.name);
-    this.returnRandomNum(sortAry);
-  },
-
-  addPeopleName(targetAry) { //viewに担当者を渡し、localstorageに登録
-    this.syoki = targetAry[0].name + 'さん';
-    this.shikai = targetAry[1].name + 'さん';
-    this.msg = 'よろしくおねがいします(๑╹ω╹๑ )';
-
-    this.beforePeoplesList = [targetAry[0].name + 'さん', targetAry[1].name + 'さん'];
-
-    const emptyObj = [];
-    emptyObj.push(this.beforePeoplesList);
-    const setItem = JSON.stringify(this.beforePeoplesList);
-    localStorage.setItem('name', this.beforePeoplesList);   
-  }
- },
 }
 
 
